41caa53612e5d248bf35fe450752f1c640eb9e3a
查无此人 <woshi123qaq@163.com>
2024-06-27 08:45:59 +0800

    test

--- src/DAO/UniversitryDAO.java
+++ src/DAO/UniversitryDAO.java
@@ -10,6 +10,15 @@ public class UniversitryDAO {
         PStat.setInt(1,u.getUniversity_id());
         PStat.setString(2,u.getName());
         PStat.setString(3,u.getLocation());
+        int row = PStat.executeUpdate();
+        if(row>0) {
+            System.out.println("success insert");
+        }
+        else{
+            System.out.println("fail to insert");
+        }
+        PStat.close();
+        return;
     }
 
     public void Upadte(model.University u,Connection conn) {
