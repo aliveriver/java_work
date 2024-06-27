e6fcd3644744362171861ad653414be03f458dc8
查无此人 <woshi123qaq@163.com>
2024-06-27 09:46:48 +0800

    test

--- src/DAO/UniversitryDAO.java
+++ src/DAO/UniversitryDAO.java
@@ -1,35 +1,94 @@
 package DAO;
 
+import model.University;
+
 import java.sql.*;
 
 public class UniversitryDAO {
 
-    public void Create(model.University u,Connection conn) throws Exception{
-        String sql = "INSERT INTO universities (university_id,name,location) VALUES(?,?,?)";
-        PreparedStatement PStat = conn.prepareStatement(sql);
-        PStat.setInt(1,u.getUniversity_id());
-        PStat.setString(2,u.getName());
-        PStat.setString(3,u.getLocation());
-        int row = PStat.executeUpdate();
-        if(row>0) {
-            System.out.println("success insert");
+    public void Create(model.University u){
+        try {
+            String sql = "INSERT INTO universities (university_id,name,location) VALUES(?,?,?)";
+            Database.setConnection();
+            Connection conn = Database.getConnection();
+            PreparedStatement PStat = conn.prepareStatement(sql);
+            PStat.setInt(1, u.getUniversity_id());
+            PStat.setString(2, u.getName());
+            PStat.setString(3, u.getLocation());
+            int row = PStat.executeUpdate();
+            if (row > 0) {
+                System.out.println("success insert");
+            } else {
+                System.out.println("fail to insert");
+            }
+            PStat.close();
+            Database.closeConnection();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        else{
-            System.out.println("fail to insert");
+    }
+
+    public void Upadte(model.University u){
+        try {
+            Database.setConnection();
+            Connection conn = Database.getConnection();
+            String select_sql = "SELECT * FROM universities WHERE university_id = ?";
+            PreparedStatement SPStat = conn.prepareStatement(select_sql);
+            SPStat.setInt(1, u.getUniversity_id());
+            ResultSet Srow = SPStat.executeQuery();
+            if (Srow.getRow()!=1) {
+                System.out.println("Fail to Select");
+                throw new Exception("ID is not found or not unique");
+            }
+            SPStat.close();
+
+            String update_sql = "UPDATE universities set name=?,location=? where university_id = ?";
+            PreparedStatement UPStat = conn.prepareStatement(update_sql);
+            UPStat.setInt(1, u.getUniversity_id());
+            UPStat.setString(2, u.getName());
+            UPStat.setString(3, u.getLocation());
+            int row = UPStat.executeUpdate();
+            if (row > 0) {
+                System.out.println("success update");
+            } else {
+                System.out.println("fail to update");
+            }
+            UPStat.close();
+            Database.closeConnection();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        PStat.close();
-        return;
     }
 
-    public void Upadte(model.University u,Connection conn) {
+    public model.University SelectById(int id) {
+        University test = new University();
+        try {
+            Database.setConnection();
+            Connection conn = Database.getConnection();
+            String select_sql = "SELECT * FROM universities WHERE university_id = ?";
+            PreparedStatement SPStat = conn.prepareStatement(select_sql);
+            SPStat.setInt(1, id);
+            ResultSet Srow = SPStat.executeQuery();
+            if (Srow.getRow()!=1) {
+                System.out.println("Fail to Select");
+                throw new Exception("ID is not found or not unique");
+            }
+            test.setLocation(Srow.getString("location"));
+            test.setName(Srow.getString("name"));
+            test.setUniversity_id(Srow.getInt("university_id"));
+            SPStat.close();
+            Database.closeConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return test;
+    }
+
+    public void SelectAll() {
 
     }
 
-    public void SelectById(int id,Connection conn) {
-
-    }
-
-    public void Delete(model.University u,Connection conn) {
+    public void Delete(model.University u) {
 
     }
 }
---
+++ src/Menu/Menu.java
@@ -0,0 +1,47 @@
+package Menu;
+
+import java.util.Scanner;
+
+public class Menu {
+    void Menu()
+    {
+        //菜单栏
+        System.out.println("欢迎来到“模拟志愿填报系统”");
+        System.out.println("1. 学生填报志愿");
+        System.out.println("2. 查看学生志愿录取情况");
+        System.out.println("3. 查看学生分班情况");
+        System.out.println("4. 查看学生专业情况");
+        System.out.println("5. 查看学生专业课程");
+        System.out.println("6. 查看所有大学");
+        System.out.println("7. 查看大学下的所有专业");
+        System.out.println("8. 查看大学所有录取的学生");
+        System.out.println("9. 查看所有学生录取情况");
+        System.out.print("请选择: ");
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        switch (choice)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            default:
+                break;
+
+        }
+    }
+}
