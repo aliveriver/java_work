72230d0fb4180829c55abc07723a077e597c6fd3
查无此人 <woshi123qaq@163.com>
2024-06-28 12:58:06 +0800

    test

---
+++ src/Service/StudentService.java
@@ -0,0 +1,30 @@
+package Service;
+
+import DAO.StudentDAO;
+import model.*;
+
+import java.util.ArrayList;
+
+abstract public class StudentService {
+    private static StudentDAO ss = new StudentDAO();
+
+    public static void Create(model.Student s){
+        ss.Create(s);
+    }
+
+    public static void Upadte(model.Student s){
+        ss.Upadte(s);
+    }
+
+    public static model.Student SelectById(int id){
+        return ss.SelectById(id);
+    }
+
+    public static ArrayList<Student> SelectAll() {
+        return ss.SelectAll();
+    }
+
+    public static void DeleteById(int id){
+        ss.DeleteById(id);
+    }
+}
