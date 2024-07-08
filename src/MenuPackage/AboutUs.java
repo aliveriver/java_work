cfca77c9cce887816fd03dae7cb3c337b236a311
Lentinel <wnddcw666@outlook.com>
2024-07-08 13:33:14 +0800

    志愿大学上限

---
+++ src/MenuPackage/AboutUs.java
@@ -0,0 +1,11 @@
+package MenuPackage;
+
+public class AboutUs {
+
+    public static void TeamInfo() {
+        System.out.println("大连理工大学软件学院 2023~2024学年第三学期 Java高级编程与应用 课程设计：");
+        System.out.println("青春猪头少年不会梦到志愿报完就分班系统");
+        System.out.println("成员：李林溪 曹子策 段凯迪 陶成文 张文海");
+    }
+
+}
--- src/MenuPackage/Menu.java
+++ src/MenuPackage/Menu.java
@@ -21,15 +21,16 @@ abstract public class Menu {
         boolean left = true;
         //菜单栏
         while (left) {
-            System.out.println("欢迎来到“模拟志愿填报系统”");
+            System.out.println("欢迎来到“模拟志愿填报系统”！");
             System.out.println("请注意，请确保所有同学提交完志愿后再分配志愿！");
             System.out.println("1. 学生信息管理");//1.添加学生信息 2.修改学生信息 3.查看学生分班情况 4.查看学生专业情况 5.查看学生专业课程
             System.out.println("2. 学生志愿管理");//学生志愿信息增删改查系统
             System.out.println("3. 学生录取情况查询");//大学学生的admission
             System.out.println("4. 大学和专业查看");// 查看所有大学  查看大学下的所有专业 3.查看大学的录取名单
-            System.out.println("5. 管理员系统功能");// 1.分配志愿信息  2.分配班级信息 3.添加大学信息 4.添加专业信息（外码参考）
-            System.out.println("6. 退出系统");
-            System.out.print("请选择: ");
+            System.out.println("5. 管理员功能");// 1.分配志愿信息  2.分配班级信息 3.添加大学信息 4.添加专业信息（外码参考）
+            System.out.println("6. 关于本系统");
+            System.out.println("7. 退出系统");
+            System.out.print("请输入选项前的数字以进入对应模块: ");
             Scanner sc = new Scanner(System.in);
             int choice = sc.nextInt();
             switch (choice) {
@@ -40,7 +41,7 @@ abstract public class Menu {
                     ApplicationController.ApplicationController();//进入志愿填报管理系统
                     break;
                 case 3:
-                    ViewAdmission.ViewAdmissionFunction();//
+                    ViewAdmission.ViewAdmissionFunction();//小亮给他整个活
                     break;
                 case 4:
                     UniversityController.UniversityControllerMenu();//进入大学管理的Menu
@@ -49,14 +50,17 @@ abstract public class Menu {
                     ManagerController.ManagerControllerMenu();//进入管理员Menu
                     break;
                 case 6:
+                    AboutUs.TeamInfo();
+                    break;
+                case 7:
                     left = false;
                     break;
                 default:
-                    System.out.println("输入有误，请重新输入");
+                    System.out.println("请输入正确选项！");
                     break;
             }
         }
-        System.out.println("欢迎下次使用志愿填报系统")  ;
+        System.out.println("============欢迎下次使用！============")  ;
 
     }
 
